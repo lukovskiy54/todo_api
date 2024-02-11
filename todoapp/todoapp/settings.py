0877cc74f58df6ddc37a7103ca148b2bd1bb2ec8
Max Lukovskiy <lostlonginus9sp@gmail.com>
2024-02-11 23:32:36 +0200

    req.txt

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vdh02&hysygezytyj$z-=moy!uvab$33(9bgwaxa0m4nkihrh4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
